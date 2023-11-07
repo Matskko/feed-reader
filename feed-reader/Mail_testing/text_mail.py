e8af9359392dc8e080f2e77f23ce4063d56838a4
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-11-07 11:11:27 +0100

    gmail//fast commit

--- feed-reader/Mail_testing/text_mail.py
+++ feed-reader/Mail_testing/text_mail.py
@@ -4,16 +4,22 @@ import smtplib
 HOST = "smtp-mail.outlook.com"
 PORT = 587
 
-FROM_EMAIL = "tengapanda@gmail.com"
+FROM_EMAIL = "tobiasprogramming@outlook.com"  
 TO_EMAIL = "tobiasvdvaart@gmail.com"
 PASSWORD = getpass.getpass("Enter password: ")
 
-MESSAGE = "Test e-mail"
+MESSAGE = """Subject: mail sent using python
+hi test,
+
+this is a test e-mail
+thanks,
+test account
+"""
 
 smtp = smtplib.SMTP(HOST, PORT)
 
 status_code, response = smtp.ehlo()
-print(f"[*] echoing the server {status_code} {response}")
+print(f"[*] Echoing the server {status_code} {response}")
 
 status_code, response = smtp.starttls()
 print(f"[*] Starting TLS {status_code} {response}")
@@ -23,4 +29,3 @@ print(f"[*] Login {status_code} {response}")
 
 smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
 smtp.quit()
-
