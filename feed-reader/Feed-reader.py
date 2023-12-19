4f8cb8fe8e6a74a688bf880b63793ee0b7a21b1f
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-12-19 13:28:38 +0100

    fast save

--- feed-reader/Feed-reader.py
+++ feed-reader/Feed-reader.py
@@ -4,7 +4,24 @@ import feedparser
 from datetime import datetime, timedelta
 import pytz
 
+# Importeer de benodigde modules
+
 def check_feed_updates(feed_url, interval):
+    """
+    Controleer op updates in een opgegeven feed URL binnen een opgegeven tijdsinterval.
+
+    Args:
+        feed_url (str): De URL van de feed om updates te controleren.
+        interval (int): Het aantal dagen om terug te kijken voor updates.
+
+    Returns:
+        list: Een lijst van dictionaries met informatie over de bijgewerkte items.
+            Elke dictionary bevat de volgende sleutels:
+            - 'update_date': De datum en tijd van de update in het formaat "%Y-%m-%d %H:%M:%S".
+            - 'title': De titel van het bijgewerkte item.
+            - 'summary': De samenvatting van het bijgewerkte item.
+            - 'link': De link naar het bijgewerkte item.
+    """
     feed = feedparser.parse(feed_url)
 
     current_date = datetime.now(pytz.utc)
@@ -35,6 +52,7 @@ def check_feed_updates(feed_url, interval):
     return info
 
 def get_urls():
+    # Lijst van feed URLs om te controleren
     urls = [
         'https://github.com/acryldata/datahub-helm/releases.atom',
         'http://github.com/datahub-project/datahub/releases.atom'
@@ -47,7 +65,7 @@ def send_email(subject, message):
     TO_EMAIL = "tobiasvdvaart@gmail.com"  
 
     if FROM_EMAIL is None or PASSWORD is None:
-        print("Email data not found in environment variables.")
+        print("E-mailgegevens niet gevonden in omgevingsvariabelen.")
         return
 
     SMTP_SERVER = "smtp.office365.com"
@@ -62,25 +80,26 @@ def send_email(subject, message):
         smtp.sendmail(FROM_EMAIL, TO_EMAIL, msg)
 
 if __name__ == "__main__":
+    # Haal het tijdsinterval op uit de omgevingsvariabele INTERVAL_IN_DAYS, standaard is 11 dagen
     interval = int(os.getenv("INTERVAL_IN_DAYS", default="11"))
     feed_urls = get_urls()
 
     all_feed_updates = []
 
     for feed_url in feed_urls:
+        # Controleer op updates in elke feed URL
         feed_updates = check_feed_updates(feed_url, interval)
         all_feed_updates.extend(feed_updates)
 
     email_subject = "Feed Updates"
-    email_body = "List of Updates:\n\n"
+    email_body = "Lijst met updates:\n\n"
 
     for update in all_feed_updates:
-        email_body += f"Update found on {update['update_date']}\n"
-        email_body += f"Title: {update['title']}\n"
-        email_body += f"Summary: {update['summary']}\n"
+        # Bouw de e-mailinhoud op met informatie over de updates
+        email_body += f"Update gevonden op {update['update_date']}\n"
+        email_body += f"Titel: {update['title']}\n"
+        email_body += f"Samenvatting: {update['summary']}\n"
         email_body += f"Link: {update['link']}\n\n"
         
     send_email(email_subject, email_body)
-    print("The E-mail has been sent.")
-    
-    
+    print("De e-mail is verstuurd.")
